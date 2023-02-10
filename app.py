342d254c73c5789d38cdd8a8e4cf96a5a4efdda1
Devmani Maurya <devmani.official19@gmail.com>
2023-02-10 02:48:03 -0500

    add introduction to home page

--- app.py
+++ app.py
@@ -138,8 +138,7 @@ def load_startup_analysis(input_startup):
     index=0
     for i in temp_df['investors']:  # to seperate all investors
         for j in i:
-            
-            investors=i
+        
             round=temp_df['round'].tolist()
             date=temp_df['date'].tolist()
 
@@ -243,7 +242,19 @@ def load_investor_details(investor):
 
    
 #start of the project---------------------------------------------------------------------------------------------------------------------------
+st.title('INDIAN STARTUPS ANALYSIS')
+st.subheader('INTRODUCTION')
+st.write("""
+This project aims to analyze the growth and trends of Indian startups from 2015 to 2020. Using streamlit, Python, pandas, and data visualization, the project will provide insights into the Indian startup ecosystem and its evolution over the years.
 
+The data for this project will be collected from various sources such as industry reports and databases, and will include information such as the number of startups, funding received, and sector-wise distribution.
+
+With the help of Python's pandas library, the collected data will be cleaned, transformed, and organized into a structured format, making it easier to analyze and visualize. The streamlit library will be used to build an interactive web-based interface to display the results and insights derived from the analysis.
+
+Data visualization techniques, such as line charts, bar graphs, and pie charts, will be used to effectively communicate the results and provide a clear understanding of the trends and patterns in the Indian startup ecosystem.
+
+This project will provide valuable insights into the Indian startup landscape, its growth and evolution, and the key players and trends that are shaping the future of startups in India. The insights derived from this project can be used by entrepreneurs, investors, and policy makers to make informed decisions and drive the growth of the Indian startup ecosystem.
+""")
 st.sidebar.title('START FUNDING ANALYSIS')
 
 option = st.sidebar.selectbox('TYPE OF ANALYSIS',['Select One','Overall Analysis','StartUp','Investors'])
